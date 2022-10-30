1d58898ce2bb13e59e8355a5710b3e1bf8044e58
Michael Beavitt <s1653324@bioinfmsc8.bio.ed.ac.uk>
2022-10-30 11:58:13 +0000

    Halfway finished, added percentage calculation with rounding.

--- intronsplice.py
+++ intronsplice.py
@@ -1,10 +1,18 @@
 #!/bin/python3
 
+# Getting sequence, setting variables
 my_dna = open("DNA_file").read()
-my_dna = my_dna.replace('\n', '')
 coding_dna = ''
-coding_dna = coding_dna + my_dna[0:64]
-print(coding_dna)
-print(len(coding_dna))
 
-print(my_dna)
+# Tidying input
+my_dna = my_dna.replace('\n', '')
+
+# Splicing
+coding_dna = coding_dna + my_dna[0:63] + my_dna[90:]
+
+# Finding rounded percentage coding
+perc_coding = round((len(coding_dna)/len(my_dna)*100))
+
+# Outputs
+print("The spliced coding sequence is: " + coding_dna)
+print("The percentage of the sequence that is coding is: " + str(perc_coding) + "%")
